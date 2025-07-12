53df4e035a89e83254e700d7ba3db582a9cef14f
GeniusMind9999 <meetgandhi13062005@gmail.com>
2025-07-12 15:52:36 +0530

    fix: formatted code and removed infinite loop

--- frontend/src/Components/Home/HomeFeed.js
+++ frontend/src/Components/Home/HomeFeed.js
@@ -1,27 +1,27 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
-import Question from './Question';
+import React, { useEffect, useState } from "react";
+import Question from "./Question";
 
 const HomeFeed = () => {
-  const [questionsData, setQuestionsData] = useState([]);
+	const [questionsData, setQuestionsData] = useState([]);
 
-  useEffect(() => {
-    const fetchQuestions = async() => {
-      const res = await fetch("http://localhost:9000/guest/questions");
-      const questions = await res.json();
-      setQuestionsData(questions.data);
-    }
-    fetchQuestions();
-  });
+	useEffect(() => {
+		const fetchQuestions = async () => {
+			const res = await fetch("http://localhost:9000/guest/questions");
+			const questions = await res.json();
+			setQuestionsData(questions.data);
+		};
+		fetchQuestions();
+	}, []);
 
-  return (
-    <div className='home-feed flex flex-col gap-5 mt-10'>
-      {questionsData.map((question,index) => (
-        <Question key={index} data={question}/>
-      ))}
-    </div>
-  )
-}
+	return (
+		<div className="home-feed flex flex-col gap-5 mt-10">
+			{questionsData.map((question, index) => (
+				<Question key={index} data={question} />
+			))}
+		</div>
+	);
+};
 
-export default HomeFeed
+export default HomeFeed;
--- frontend/src/app/(routes)/add-question/page.jsx
+++ frontend/src/app/(routes)/add-question/page.jsx
@@ -1,80 +1,86 @@
-'use client';
+"use client";
 
-import React,{useState,useEffect} from 'react';
-import Editor from '../../../Components/editor';
+import React, { useState, useEffect } from "react";
+import Editor from "../../../Components/editor";
 
-import {useRouter} from 'next/navigation';
-
-
+import { useRouter } from "next/navigation";
 
 export default function AskQuestion() {
-  const router=useRouter();
-    const [title,setTitle]=useState("");
-    const[description,setDescription]=useState("");
-    const[tags,setTags]=useState("");
-    const handsubmit=async(e)=>{
-        e.preventDefault();
-         const parser = new DOMParser();
-    const doc = parser.parseFromString(description, "text/html");
+	const router = useRouter();
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
+	const [tags, setTags] = useState("");
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const parser = new DOMParser();
+		const doc = parser.parseFromString(description, "text/html");
 
-    const base64Images = Array.from(doc.querySelectorAll("img"))
-  .map(img => img.src)
-  .filter(src => src.startsWith("data:image"));
-  const tagsList = tags.trim().split(/\s+/);
-  const plainTextDescription = doc.body.innerText;
-    await fetch("http://localhost:9000/user/create/question", {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ title:title, content: plainTextDescription, tags:tagsList,images:base64Images }),
-    });
-    router.push('/');
-    }
+		const base64Images = Array.from(doc.querySelectorAll("img"))
+			.map((img) => img.src)
+			.filter((src) => src.startsWith("data:image"));
+		const tagsList = tags.trim().split(/\s+/);
+		const plainTextDescription = doc.body.innerText;
+		await fetch("http://localhost:9000/user/create/question", {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				title: title,
+				content: plainTextDescription,
+				tags: tagsList,
+				images: base64Images,
+			}),
+		});
+		router.push("/");
+	};
 
-  return (
-    <div className="font-funnelDisplay px-20 bg-gray-100 p-5">
-        <form className="max-w-full mx-auto p-6 bg-white rounded-2xl shadow-lg mb-6" onSubmit={handsubmit}>
-          <h2 className="text-2xl font-bold mb-6 text-gray-800">Ask a Question</h2>
+	return (
+		<div className="font-funnelDisplay px-20 bg-gray-100 p-5">
+			<form
+				className="max-w-full mx-auto p-6 bg-white rounded-2xl shadow-lg mb-6"
+				onSubmit={(e) => handleSubmit(e)}
+			>
+				<h2 className="text-2xl font-bold mb-6 text-gray-800">Ask a Question</h2>
 
-          {/* Title Input */}
-          <div className="mb-5">
-            <label className="block text-gray-700 font-medium mb-2">Title</label>
-            <input
-              type="text"
-              placeholder="Title"
-              value={title}
-              onChange={(e)=>setTitle(e.target.value)}
-              className="w-4/6 px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+				{/* Title Input */}
+				<div className="mb-5">
+					<label className="block text-gray-700 font-medium mb-2">Title</label>
+					<input
+						type="text"
+						placeholder="Title"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+						className="w-4/6 px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+					/>
+				</div>
 
-          {/* Description Editor */}
-          <div className="mb-5">
-            <label className="block text-gray-700 font-medium mb-2">Description</label>
-            <div className="bg-white border border-gray-300 rounded-xl p-2">
-              <Editor onChange={setDescription}/>
-            </div>
-          </div>
+				{/* Description Editor */}
+				<div className="mb-5">
+					<label className="block text-gray-700 font-medium mb-2">Description</label>
+					<div className="bg-white border border-gray-300 rounded-xl p-2">
+						<Editor onChange={setDescription} />
+					</div>
+				</div>
 
-          {/* Tags Input */}
-          <div className="mb-6">
-            <label className="block text-gray-700 font-medium mb-2">Tags</label>
-            <input
-              type="text"
-              placeholder="Tags"
-              value={tags}
-              onChange={e=>setTags(e.target.value)}
-              className="w-4/6 px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+				{/* Tags Input */}
+				<div className="mb-6">
+					<label className="block text-gray-700 font-medium mb-2">Tags</label>
+					<input
+						type="text"
+						placeholder="Tags"
+						value={tags}
+						onChange={(e) => setTags(e.target.value)}
+						className="w-4/6 px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+					/>
+				</div>
 
-          {/* Submit Button */}
-          <button
-            type="submit"
-            className="w-1/4 bg-green-300 text-white py-3 rounded-xl hover:bg-blue-700 transition duration-300 font-semibold"
-          >
-            Submit
-          </button>
-      </form>
-    </div>
-  );
+				{/* Submit Button */}
+				<button
+					type="submit"
+					className="w-1/4 bg-green-300 text-white py-3 rounded-xl hover:bg-blue-700 transition duration-300 font-semibold"
+				>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 }
