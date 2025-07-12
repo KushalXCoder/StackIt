99319f45270883663ec5f37cdf23e3270edc7b08
GeniusMind9999 <meetgandhi13062005@gmail.com>
2025-07-12 12:09:38 +0530

    feat: added createAnswer and createComment

--- Backend/Schemas/comment.schema.js
+++ Backend/Schemas/comment.schema.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
-const User = require("./user.schema");
-const Answer = require("./answer.schema");
 
 const commentSchema = new mongoose.Schema(
 	{
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const createQuestion = require("../controllers/users/createQuestion.controller");
+const createAnswer = require("../controllers/users/createAnswer.controller");
+const createComment = require("../controllers/users/createComment.controller");
 
 const userRouter = express.Router();
 
 userRouter.put("/create/question", createQuestion);
+userRouter.put("/create/answer", createAnswer);
+userRouter.put("/create/comment", createComment);
 
 module.exports = userRouter;
