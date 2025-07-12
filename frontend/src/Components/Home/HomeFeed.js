7a7d6a0d75de09aec9c39468c261480959d662f5
Kushal Rathod <kushalrathod044@gmail.com>
2025-07-12 15:12:04 +0530

    Changes

--- frontend/src/Components/Home/HomeFeed.js
+++ frontend/src/Components/Home/HomeFeed.js
@@ -1,7 +1,20 @@
-import React from 'react';
+"use client";
+
+import React, { useEffect, useState } from 'react';
 import Question from './Question';
 
 const HomeFeed = () => {
+  const [questionsData, setQuestionsData] = useState([]);
+
+  useEffect(() => {
+    const fetchQuestions = async() => {
+      const res = await fetch("http://localhost:9000/guest/getQuestions");
+      const data = await res.json();
+      console.log(data);
+    }
+    fetchQuestions();
+  });
+
   return (
     <div className='home-feed flex flex-col gap-5 mt-10'>
         <Question/>
